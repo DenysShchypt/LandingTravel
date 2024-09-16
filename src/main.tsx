82f18066cd850a126f86d360d4e653202105f730
DenysShchypt <denysshchyptsovit@gmail.com>
2024-09-16 22:50:48 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { Provider } from 'react-redux'
 import App from './App'
 import store from './store'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
