511e69d703f35207ebde05519d04239ca11ec1bd
DenysShchypt <denysshchyptsovit@gmail.com>
2024-09-23 23:20:11 +0300

    fix bug

--- src/components/Header/Hero/FilterMobile/index.tsx
+++ src/components/Header/Hero/FilterMobile/index.tsx
@@ -19,40 +19,40 @@ const FilterMobile: FC = (): JSX.Element => {
   };
 
   return (
-    <AnimatePresence>
-      {isVisible && (
-        <motion.div
-          initial={{ opacity: 0.5, x: "100%" }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0.5, x: "100%" }}
-          transition={{ duration: 0.5 }}
-          className="fixed top-0 right-0 h-full w-full bg-white px-8 pt-[72px] xl:hidden"
+<AnimatePresence>
+  {isVisible && (
+    <motion.div
+      initial={{ opacity: 1, x: "100%" }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0.5, x: "100%" }}
+      transition={{ duration: 0.5 }}
+      className="fixed top-0 right-0 h-screen w-screen bg-white px-8 pt-[72px] xl:hidden"
+    >
+      <div className="flex justify-between items-center py-4 mb-10">
+        <h2 className="font-medium text-xl text-black">{nameSelect}</h2>
+        <button
+          onClick={handleCloseMobileSelect}
+          className={`${
+            nameSelect === ""
+              ? "flex items-center justify-center rounded-full border-[1px] border-auxiliary w-12 h-12"
+              : ""
+          }`}
         >
-          <div className="flex justify-between items-center py-4 mb-10">
-            <h2 className="font-medium text-xl text-black">{nameSelect}</h2>
-            <button
-              onClick={handleCloseMobileSelect}
-              className={`${
-                nameSelect === ""
-                  ? "flex items-center justify-center rounded-full border-[1px] border-auxiliary   w-12 h-12"
-                  : ""
-              }`}
-            >
-              <svg
-                className={`${
-                  nameSelect === "" ? "fill-auxiliary" : "fill-black"
-                } stroke-1`}
-                width="20"
-                height="14"
-              >
-                <use href="./icons.svg#icon-closed"></use>
-              </svg>
-            </button>
-          </div>
-          <FilterMobileComponents/>
-        </motion.div>
-      )}
-    </AnimatePresence>
+          <svg
+            className={`${
+              nameSelect === "" ? "fill-auxiliary" : "fill-black"
+            } stroke-1`}
+            width="20"
+            height="14"
+          >
+            <use href="./icons.svg#icon-closed"></use>
+          </svg>
+        </button>
+      </div>
+      <FilterMobileComponents />
+    </motion.div>
+  )}
+</AnimatePresence>
   );
 };
 
